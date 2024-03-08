33596ce229029d60fe91835d4277ca41941aaf10
amine9005 <amamselmitrader@gmail.com>
2024-03-08 23:35:49 +0100

    mobile header update

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,37 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./header.scss"
 import { Link } from 'react-router-dom'
 
-const header = () => {
+export const Header = () => {
+    const [mobile,setMobile] = useState(false)
   return (
-    <><header >
+    <>
+<header>
         <div className='container flexSB'>
-            <nav className='flexSB'>
-                <div className="logo">
-                    {/* <img src='../images/logo.png'></img> */}
-                </div>
-                <ul className='flexSB'>
-                    <Link to='/'>Home</Link>
-                    <Link to='/series'>Series</Link>
-                    <Link to='/movies'>Movies</Link>
-                    <Link to='/pages'>Pages</Link>
-                    <Link to='/pricing'>Pricing</Link>
-                    <Link to='/contact'>Contact</Link>
-                </ul>
-
-                <button className='toggle'>
-                    <i className='fa fa-bars'></i>
-                </button>
-            </nav>
-            <div className="account flexSB">
-                    <i className='fa fa-search'></i>
-                    <i className='fa fa-bell'></i>
-                    <i className='fa fa-user'></i>
-                    <button> Subscribe Now</button>
-                </div>
+          <nav className='flexSB'>
+            <div className='logo'>
+              <img src='./images/logo.png' alt='' />
+            </div>
+            {/*<ul className='flexSB'>*/}
+            <ul className={mobile ? "navMenu-list" : "flexSB"} onClick={() => setMobile(false)}>
+              <li>
+                <a href='/'>Home</a>
+              </li>
+              <li>
+                <a href='/'>Series</a>
+              </li>
+              <li>
+                <a href='/'>Movies</a>
+              </li>
+              <li>
+                <a href='/'>Pages</a>
+              </li>
+              <li>
+                <a href='/'>Pricing</a>
+              </li>
+              <li>
+                <a href='/'>Contact</a>
+              </li>
+            </ul>
+            <button className='toggle' onClick={() => setMobile(!mobile)}>
+              {mobile ? <i className='fa fa-times'></i> : <i className='fa fa-bars'></i>}
+            </button>
+          </nav>
+          <div className='account flexSB'>
+            <i className='fa fa-search'></i>
+            <i className='fas fa-bell'></i>
+            <i className='fas fa-user'></i>
+            <button>Subscribe Now</button>
+          </div>
         </div>
-    </header></>
+      </header>
+    </>
   )
 }
 
-export default header
+export default Header
