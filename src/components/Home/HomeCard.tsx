47085aedee33f236b9b9366851fe7c12c57ba694
amine9005 <amamselmitrader@gmail.com>
2024-03-11 23:50:56 +0100

    HomeCards Update

--- src/components/Home/HomeCard.tsx
+++ src/components/Home/HomeCard.tsx
@@ -1,12 +1,34 @@
 import React from 'react'
 
 interface item {
-  id:String,cover:String,name:String,rating:Number,time:Date,desc:String,starring:String,genres:String
+  id:string,cover:string,name:string,rating:number,time:string,desc:string,starring:string,genres:string,tags:Array<String>,video:string
 }
 
-const HomeCard = (item:item) => {
+const HomeCard = (props:{item:item}) => {
   return (
-    <div>HomeCard</div>
+    <div className='box'>
+      <div className="coverImage">
+        <img src={props.item.cover} alt=''/>
+      </div>
+      <div className="content flex">
+        <h1>{props.item.name}</h1>
+        <div className="rating flex">
+          <i className='fa fa-star'></i>
+          <i className='fa fa-star'></i>
+          <i className='fa fa-star'></i>
+          <i className='fa fa-star-half'></i>
+        </div>
+        <label>{props.item.rating}</label>
+        <span>GP</span>
+        <label>{props.item.time}</label>
+        <p>{props.item.desc}</p>
+        <div className='cast'>
+          <h4><span>Starring</span>{props.item.starring}</h4>
+          
+        </div>
+      </div>
+
+    </div>
   )
 }
 
