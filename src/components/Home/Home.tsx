52ca904b5c5589203ea7e5d5a8511ddf5990fc95
amine9005 <amamselmitrader@gmail.com>
2024-03-12 23:53:50 +0100

    home page updates

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { homeData } from '../../dummyData'
 import Page from './Page'
+import './Home.scss'
 
 const Home = () => {
   const [items, setItems] = React.useState(homeData)
--- src/components/Home/HomeCard.tsx
+++ src/components/Home/HomeCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 interface item {
   id:string,cover:string,name:string,rating:number,time:string,desc:string,starring:string,genres:string,tags:Array<String>,video:string
@@ -23,8 +24,37 @@ const HomeCard = (props:{item:item}) => {
         <label>{props.item.time}</label>
         <p>{props.item.desc}</p>
         <div className='cast'>
-          <h4><span>Starring</span>{props.item.starring}</h4>
-          
+          <h4>
+            <span>Starring</span>
+            {props.item.starring}
+          </h4>
+
+          <h4>
+            <span>Genres</span>
+            {props.item.genres}
+          </h4>
+
+          <h4>
+            <span>Tags</span>
+            {props.item.tags}
+          </h4>
+
+          <button className='primary-btn'>
+              <i className='fa fa-play'></i>
+          </button>
+
+          <div className='button-row'>
+            <Link to={`/singlePage/${props.item.id}`}></Link>
+            <button >
+              <div className='img'>
+              <img src='./images/play-button.png' alt='' />
+              <img src='./images/play.png' alt='' />
+              </div>
+              WATCH TRAILER
+            </button>
+
+          </div>
+
         </div>
       </div>
 
