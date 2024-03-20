6119ae4632ce6e8c7ace45a3fb5951984ee1dd9b
amine9005 <amamselmitrader@gmail.com>
2024-03-20 23:34:14 +0100

    home page updates

--- src/components/Home/Page.tsx
+++ src/components/Home/Page.tsx
@@ -4,6 +4,30 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
+
+const SampleNextArrow = (props:any) =>{
+  const {onClick} = props
+
+  return (
+    <div className='control-btn' onClick={onClick}>
+      <button className='next'>
+        <i className='fa fa-chevron-right'></i>
+      </button>
+    </div>
+  )
+}
+
+const SamplePrevArrow = (props:any) =>{
+  const {onClick} = props
+
+  return (
+    <div className='control-btn' onClick={onClick}>
+      <button className='prev'>
+        <i className='fa fa-chevron-left'></i>
+      </button>
+    </div>
+  )
+}
 const Page = (items:any) => {
   const settings = {
     dots: true,
@@ -11,6 +35,9 @@ const Page = (items:any) => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    nextArrow:<SampleNextArrow/>,
+    prevArrow:<SamplePrevArrow/>
+
   };
 
 
