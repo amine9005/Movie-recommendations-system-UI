3eb64ef2e36da6e6927ceae4c7d1662141020094
amine9005 <amamselmitrader@gmail.com>
2024-03-22 23:49:34 +0100

    upcoming scss updates

--- src/components/Upcoming/UCard.tsx
+++ src/components/Upcoming/UCard.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const UCard = (props:{item:any}) => {
+const UCard = (props:{item:{id:string,cover:string,name:string,time:string}}) => {
   return (
-    <div>UCard</div>
+    <>
+      <div className='movieBox'>
+        <div className="img">
+          <img src={props.item.cover} alt={"cover not available: "+props.item.cover}/>
+        </div>
+        <div className="text">
+          <h3>{props.item.name}</h3>
+          <span>{props.item.time}</span>
+          <button className="primary-btn">
+            <i className='fa fa-play'></i> Play Now
+          </button>
+        </div>
+      </div>
+    </>
   )
 }
 
