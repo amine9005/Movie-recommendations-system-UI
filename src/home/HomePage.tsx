6a0734be8d2b7855ea311b374dbd404a4b69f610
amine9005 <amamselmitrader@gmail.com>
2024-03-28 23:36:13 +0100

    trending and recommended updates

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react'
 import Home from '../components/Home/Home'
 import Upcoming from '../components/Upcoming/Upcoming'
-import { latest,upcome } from '../dummyData';
+import { latest, upcome, recommended } from '../dummyData';
 import Trending from '../components/Trending/Trending';
 
 const HomePage = () => {
 
   const [upcoming,setUpcoming] = useState(upcome)
   const [late,setLatest] = useState(latest)
+  const [recommend , setRecommend] = useState(recommended)
   return (
     <>
       <Home/>
       <Upcoming items={upcoming} title="Upcoming Movies"/>
       <Upcoming items={late} title="Latest Movies"/>
       <Trending/>
+      <Upcoming items={recommend} title="Recommended Movies"/>
+
     </>
   )
 }
