99e82e379a7e806ec012ecb843d9d6bb38cde09f
amine9005 <amamselmitrader@gmail.com>
2024-04-05 23:50:49 +0100

    watch page update

--- src/components/Watch/Watch.tsx
+++ src/components/Watch/Watch.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { homeData } from '../../dummyData'
+import Upcoming from '../Upcoming/Upcoming'
+import { recommended } from '../../dummyData'
 
 const Watch = () => {
   const {id} = useParams()
   const [items, setItems] = useState(homeData)
+  const [recommend , setRecommend] = useState(recommended)
+
 
   useEffect(() => {
     let item = homeData.find((item) => item.id === parseInt(id!))
@@ -28,9 +32,13 @@ const Watch = () => {
                 <p>{item.desc}</p>
               </div>
               <div className='social'>
-
+              <h3>Share : </h3>
+                <img src='https://img.icons8.com/color/48/000000/facebook-new.png' />
+                <img src='https://img.icons8.com/fluency/48/000000/twitter-circled.png' />
+                <img src='https://img.icons8.com/fluency/48/000000/linkedin-circled.png' />
               </div>
             </div>
+            <Upcoming items={recommend} title="Recommended Movies"/>
 
           </section>
         </>
