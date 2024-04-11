4687c4d16c407ac7a889f85c031c2e038ef430f1
amine9005 <amamselmitrader@gmail.com>
2024-04-11 22:46:17 +0100

    tmbdClient updates

--- src/Api/tmdbClient.tsx
+++ src/Api/tmdbClient.tsx
@@ -1,48 +1,49 @@
 import axiosClient from "./axiosClient";
+import Button from '@mui/material/Button'
 
-export const category = {
+export const category:{[key: string]: string} = {
     movie: 'movie',
     tv: 'tv'
 }
 
-export const movieType = {
+export const movieType:{[key: string]: string} = {
     upcoming: 'upcoming',
     popular: 'popular',
     top_rated: 'top_rated'
 }
 
-export const tvType = {
+export const tvType:{[key: string]: string} = {
     popular: 'popular',
     top_rated: 'top_rated',
     on_the_air: 'on_the_air'
 }
 
 const tmdbApi = {
-    getMoviesList: (type, params) => {
+    getMoviesList: (type:string, params:any) => {
         const url = 'movie/' + movieType[type];
         return axiosClient.get(url, params);
     },
-    getTvList: (type, params) => {
+    getTvList: (type:string, params:any) => {
         const url = 'tv/' + tvType[type];
         return axiosClient.get(url, params);
     },
-    getVideos: (cate, id) => {
+    getVideos: (cate:string, id:string) => {
         const url = category[cate] + '/' + id + '/videos';
         return axiosClient.get(url, {params: {}});
     },
-    search: (cate, params) => {
+    search: (cate:string, params:any) => {
         const url = 'search/' + category[cate];
         return axiosClient.get(url, params);
     },
-    detail: (cate, id, params) => {
+    detail: (cate:string, id:string, params:any) => {
         const url = category[cate] + '/' + id;
         return axiosClient.get(url, params);
     },
-    credits: (cate, id) => {
+    credits: (cate:string, id:string) => {
         const url = category[cate] + '/' + id + '/credits';
         return axiosClient.get(url, {params: {}});
     },
-    similar: (cate, id) => {
+    similar: (cate:string, id:string) => {
         const url = category[cate] + '/' + id + '/similar';
         return axiosClient.get(url, {params: {}});
     },
