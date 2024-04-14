7ae6f5761a215e91bc39932e13867676f54be13b
amine9005 <amamselmitrader@gmail.com>
2024-04-14 23:33:09 +0100

    env update

--- src/Api/ApiConfig.tsx
+++ src/Api/ApiConfig.tsx
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'themoviedb.org',
+    apiKey: process.env.API_KEY,
     originalImage: (imgPath:string) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath:string) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
